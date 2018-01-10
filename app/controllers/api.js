673b68e7d6c8407d4879af6b0ec5f0cf9755908a
Ahmed Ghazi <ahmed.ghazi@soixanteseize.com>
2018-01-10 18:29:51 +0100

    retours

--- app/controllers/api.js
+++ app/controllers/api.js
@@ -204,6 +204,7 @@ router.get('/media/:id', function(req, res, next) {
             //console.log(post)
             if (!post)return res.json(post)
             if (post.link.indexOf("facebook.com") > -1)return res.json(post)
+            if (post.link.indexOf("float.gallery") > -1)return res.json(post)
             if (post.name == "tig.ht")return res.json(post)
             if (post.color && post.color != "")return res.json(post);
 
