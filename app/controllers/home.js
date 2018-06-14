4389e35d59e953df35d7292df51ef2f12c75b542
Ahmed Ghazi <ahmed.ghazi@soixanteseize.com>
2018-06-14 11:58:32 +0200

    majs

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,76 +1,76 @@
 var express = require('express'),
-  router = express.Router(),
-  mongoose = require('mongoose'),
-  Post = mongoose.model('Post'),
-  Options = mongoose.model('Options'),
-  postsPerPage;
+router = express.Router(),
+mongoose = require('mongoose'),
+Post = mongoose.model('Post'),
+Options = mongoose.model('Options'),
+postsPerPage;
 
 module.exports = function (app) {
-  postsPerPage = 30;
-  app.use('/', router);
+	postsPerPage = 30;
+	app.use('/', router);
 };
 
 router.get('/', function (req, res, next) {
-  return Post
-	  .find()
-	  .sort({updated_time: 'desc'})
-	  .limit(postsPerPage)
-	  .exec(function(err, posts) {
-	  if (err) {
-		  console.log(err);
-		  return next(err);
-	  }
-	 
-	  Options
+	return Post
+	.find()
+	.sort({updated_time: 'desc'})
+	.limit(postsPerPage)
+	.exec(function(err, posts) {
+		if (err) {
+			console.log(err);
+			return next(err);
+		}
+		
+		Options
 		.findOne({'meta.key': 'total_posts'})
 		.exec(function(err, option) {
 		  //console.log(option.meta)
 		  return res.render('index', {
-			title: 'CRAZY COOL WEBSITES Archive',
-			posts: posts,
-			total_posts: option.meta.value
+		  	title: 'CRAZY COOL WEBSITES Archive',
+		  	posts: posts,
+		  	total_posts: option.meta.value
 		  });
 		});
 	  //console.log(app.get('title'));
 	  
-  });
+	});
 });
 
 
 router.get('/images', function (req, res, next) {
-  return Post
-	  .find()
-	  .sort({updated_time: 'desc'})
-	  .limit(postsPerPage)
-	  .exec(function(err, posts) {
-	  if (err) {
-		  console.log(err);
-		  return next(err);
-	  }
-	 
-	  Options
+	return Post
+	.find()
+	.sort({updated_time: 'desc'})
+	.limit(postsPerPage)
+	.exec(function(err, posts) {
+		if (err) {
+			console.log(err);
+			return next(err);
+		}
+		
+		Options
 		.findOne({'meta.key': 'total_posts'})
 		.exec(function(err, option) {
 		  //console.log(option.meta)
 		  return res.render('images', {
-			title: 'CRAZY COOL WEBSITES Archive',
-			posts: posts,
-			total_posts: option.meta.value
+		  	title: 'CRAZY COOL WEBSITES Archive',
+		  	posts: posts,
+		  	total_posts: option.meta.value
 		  });
 		});
 	  //console.log(app.get('title'));
 	  
-  });
+	});
 });
 
 router.get('/images/page/:id', function (req, res, next) {
-  var skip = parseInt(req.params.id * postsPerPage);
-  return Post
-	  .find()
-	  .sort({updated_time: 'desc'})
-	  .limit(postsPerPage)
-	  .skip(skip)
-	  .exec(function(err, posts) {
+	var skip = parseInt(req.params.id * postsPerPage);
+	return Post
+	.find()
+	.sort({updated_time: 'desc'})
+	.limit(postsPerPage)
+	.skip(skip)
+	.exec(function(err, posts) {
 		if (err) {
 			console.log(err);
 			return next(err);
@@ -80,49 +80,49 @@ router.get('/images/page/:id', function (req, res, next) {
 		  //title: 'CRAZY COOL WEBSITES Archive',
 		  posts: posts
 		});
-  });
-  
+	});
+	
 });
 
 router.get('/contributors', function (req, res, next) {
-  return Post
-	  .find()
+	return Post
+	.find()
 	  //.sort({updated_time: 'desc'})
 	  //.limit(postsPerPage)
 	  .exec(function(err, posts) {
-	  if (err) {
-		  console.log(err);
-		  return next(err);
-	  }
-	  var tmp = [];
-	  for (var i = 0, len = posts.length; i < len; i++) {
-		tmp.push(posts[i].from);
-	  }
-	  var contributors = Array.from(new Set(tmp))
-	  contributors.sort();
-	  return res.render('contributors', {
-		title: 'CRAZY COOL WEBSITES Archive',
-		contributors: contributors,
-		total_posts: contributors.length
+	  	if (err) {
+	  		console.log(err);
+	  		return next(err);
+	  	}
+	  	var tmp = [];
+	  	for (var i = 0, len = posts.length; i < len; i++) {
+	  		tmp.push(posts[i].from);
+	  	}
+	  	var contributors = Array.from(new Set(tmp))
+	  	contributors.sort();
+	  	return res.render('contributors', {
+	  		title: 'CRAZY COOL WEBSITES Archive',
+	  		contributors: contributors,
+	  		total_posts: contributors.length
+	  	});
+	  	
 	  });
-	  
-  });
-});
+	});
 
 router.get('/about', function (req, res, next) {
-  return res.render('about', {
-	title: 'CRAZY COOL WEBSITES Archive'
-  });
+	return res.render('about', {
+		title: 'CRAZY COOL WEBSITES Archive'
+	});
 });
 
 router.get('/page/:id', function (req, res, next) {
-  var skip = parseInt(req.params.id * postsPerPage);
-  return Post
-	  .find()
-	  .sort({updated_time: 'desc'})
-	  .limit(postsPerPage)
-	  .skip(skip)
-	  .exec(function(err, posts) {
+	var skip = parseInt(req.params.id * postsPerPage);
+	return Post
+	.find()
+	.sort({updated_time: 'desc'})
+	.limit(postsPerPage)
+	.skip(skip)
+	.exec(function(err, posts) {
 		if (err) {
 			console.log(err);
 			return next(err);
@@ -132,31 +132,31 @@ router.get('/page/:id', function (req, res, next) {
 		  //title: 'CRAZY COOL WEBSITES Archive',
 		  posts: posts
 		});
-  });
-  
+	});
+	
 });
 
 router.get('/random', function (req, res, next) {
   //var skip = parseInt(req.params.id * postsPerPage);
   return Post
-	  .aggregate([
-		{ $sample: { size: postsPerPage } }
-	  ])
-	  .exec(function(err, posts) {
-		if (err) {
-			console.log(err);
-			return next(err);
-		}
-		Options
-		  .findOne({'meta.key': 'total_posts'})
-		  .exec(function(err, option) {
-			console.log(option.meta)
-			return res.render('index', {
-			  title: 'CRAZY COOL WEBSITES Archive',
-			  posts: posts,
-			  total_posts: option.meta.value
-			});
-		  });
+  .aggregate([
+  	{ $sample: { size: postsPerPage } }
+  	])
+  .exec(function(err, posts) {
+  	if (err) {
+  		console.log(err);
+  		return next(err);
+  	}
+  	Options
+  	.findOne({'meta.key': 'total_posts'})
+  	.exec(function(err, option) {
+  		console.log(option.meta)
+  		return res.render('index', {
+  			title: 'CRAZY COOL WEBSITES Archive',
+  			posts: posts,
+  			total_posts: option.meta.value
+  		});
+  	});
   });
   
 });
@@ -164,91 +164,91 @@ router.get('/random', function (req, res, next) {
 router.get('/random/page/:id', function (req, res, next) {
   //var skip = parseInt(req.params.id * postsPerPage);
   return Post
-	  .aggregate([
-		{ $sample: { size: postsPerPage } }
-	  ])
-	  .exec(function(err, posts) {
-		if (err) {
-			console.log(err);
-			return next(err);
-		}
+  .aggregate([
+  	{ $sample: { size: postsPerPage } }
+  	])
+  .exec(function(err, posts) {
+  	if (err) {
+  		console.log(err);
+  		return next(err);
+  	}
 		//console.log(app.get('title'));
 		return res.render('liste', {
-		  title: 'CRAZY COOL WEBSITES Archive',
-		  posts: posts
+			title: 'CRAZY COOL WEBSITES Archive',
+			posts: posts
 		});
-  });
+	});
   
 });
 
 router.get('/search', function (req, res, next) {
-  return res.render('search', {
-	title: 'CRAZY COOL WEBSITES Archive'
-  });
-  
+	return res.render('search', {
+		title: 'CRAZY COOL WEBSITES Archive'
+	});
+	
 });
 
 router.get('/search/:term', function (req, res, next) {
-  console.log(req.params.term)
-  return Post
-	  .find({
+	console.log(req.params.term)
+	return Post
+	.find({
 		"$or": [
-		  { name : { $regex: req.params.term, $options: 'i' }},
-		  { link : { $regex: req.params.term, $options: 'i' }},
-		  { from : { $regex: req.params.term, $options: 'i' }}
+		{ name : { $regex: req.params.term, $options: 'i' }},
+		{ link : { $regex: req.params.term, $options: 'i' }},
+		{ from : { $regex: req.params.term, $options: 'i' }}
 		]
-	  })
-	  .sort({updated_time: 'desc'})
-	  .exec(function(err, posts) {
-	  if (err) {
-		  console.log(err);
-		  return next(err);
-	  }
-	 
-	  Options
+	})
+	.sort({updated_time: 'desc'})
+	.exec(function(err, posts) {
+		if (err) {
+			console.log(err);
+			return next(err);
+		}
+		
+		Options
 		.findOne({'meta.key': 'total_posts'})
 		.exec(function(err, option) {
-		  console.log(option.meta)
-		  return res.render('index', {
-			title: 'CRAZY COOL WEBSITES Archive',
-			posts: posts,
-			total_posts: posts.length
-		  });
+			console.log(option.meta)
+			return res.render('index', {
+				title: 'CRAZY COOL WEBSITES Archive',
+				posts: posts,
+				total_posts: posts.length
+			});
 		});
 	  //console.log(app.get('title'));
 	  
-  });
-  
+	});
+	
 });
 
 router.get('/s/:term', function (req, res, next) {
   //var skip = parseInt(req.params.id * postsPerPage);
   return Post
-	  .find({
-		"$or": [
-		  { name : { $regex: req.params.term, $options: 'i' }},
-		  { link : { $regex: req.params.term, $options: 'i' }},
-		  { from : { $regex: req.params.term, $options: 'i' }}
-		]
-	  })
-	  .sort({updated_time: 'desc'})
-	  .exec(function(err, posts) {
-		if (err) {
-			console.log(err);
-			return next(err);
-		}
+  .find({
+  	"$or": [
+  	{ name : { $regex: req.params.term, $options: 'i' }},
+  	{ link : { $regex: req.params.term, $options: 'i' }},
+  	{ from : { $regex: req.params.term, $options: 'i' }}
+  	]
+  })
+  .sort({updated_time: 'desc'})
+  .exec(function(err, posts) {
+  	if (err) {
+  		console.log(err);
+  		return next(err);
+  	}
 		//console.log(app.get('title'));
 		return res.render('liste', {
-		  title: 'CRAZY COOL WEBSITES Archive',
-		  posts: posts
+			title: 'CRAZY COOL WEBSITES Archive',
+			posts: posts
 		});
-  });
+	});
   
 });
 
 router.get('/pull', function (req, res, next) {
-  return res.render('pull', {
-	title: 'pull',
-  });
+	return res.render('pull', {
+		title: 'pull',
+	});
 });
 
